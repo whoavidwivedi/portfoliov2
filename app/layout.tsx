81ef7ca5c3224705f5c0921f87045dc2a33dfc78
Avi Dwivedi <theavidwivedi@gmail.com>
2026-09-08 23:53:49 +0530

    small ui update

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,8 +4,10 @@ import type { Metadata } from "next"
 import "./globals.css"
 import { ThemeProvider } from "@/components/theme-provider"
 import { TooltipProvider } from "@/components/ui/tooltip"
-import { JetBrains_Mono } from "next/font/google";
+import { JetBrains_Mono, Public_Sans } from "next/font/google";
 import { cn } from "@/lib/utils";
+
+const publicSans = Public_Sans({subsets:['latin'],variable:'--font-sans'});
 
 const jetbrainsMono = JetBrains_Mono({subsets:['latin'],variable:'--font-mono'});
 
@@ -38,7 +40,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" suppressHydrationWarning className={cn("antialiased font-sans", jetbrainsMono.variable)}>
+    <html lang="en" suppressHydrationWarning className={cn("antialiased font-sans", jetbrainsMono.variable, "font-sans", publicSans.variable)}>
       <body>
         <TooltipProvider>
           <ThemeProvider>{children}</ThemeProvider>
